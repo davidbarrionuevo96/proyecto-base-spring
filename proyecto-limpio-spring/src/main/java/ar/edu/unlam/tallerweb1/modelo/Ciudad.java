184969cb349d9f1970417f4ee7bc430921e3ef9e
DavidBarrionuevo <david.barrionuevo96@hotmail.com>
2018-09-17 13:18:36 -0300

    Clases Agregadas

---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
@@ -0,0 +1,52 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Ciudad {
+	
+	@OneToOne
+	private Pais pais;
+	
+	@OneToOne
+	private Ubicacion ubicacionGeografica;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public Ubicacion getUbicacionGeografica() {
+		return ubicacionGeografica;
+	}
+	public void setUbicacionGeografica(Ubicacion ubicacionGeografica) {
+		this.ubicacionGeografica = ubicacionGeografica;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+	
+
+}
---
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -0,0 +1,32 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ubicacion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String longitud;
+	private String latitud;
+	
+	public String getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
+	public String getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+	
+	
+	
+}
